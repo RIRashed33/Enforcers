d687f50bd92f7dbd784fabb7034c13f6902536c5
Rashed <ri.rashed33@gmail.com>
2023-12-13 00:43:56 +0600

    home page html, owl-carousel, bootstrap js and jQuery Added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,46 @@
+(function ($){
+    "use strict";
+    $(document).ready(function(){
+
+        if($('.home-image-slider')){
+            $('.home-image-slider').owlCarousel({
+                loop:true,
+                margin:10,
+                nav: false,
+                responsive:{
+                    0:{
+                        items:1
+                    },
+                    600:{
+                        items:3
+                    },
+                    1000:{
+                        items:5
+                    }
+                }
+            })
+        };
+
+        if($('.charity-slider')){
+            $('.charity-slider').owlCarousel({
+                loop:true,
+                margin:10,
+                nav: false,
+                responsive:{
+                    0:{
+                        items:1
+                    }
+                }
+            })
+        };
+
+        
+
+
+
+    });
+}(jQuery));
+
 function navmenu_toggle(){
     document.querySelector("body").classList.toggle("show-menu");
 }
